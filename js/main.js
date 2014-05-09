38369ff588fb21dac071789d2bc908aac5eff2ca
Tom Walpole <tom@tomwalpole.com>
2014-05-08 18:37:20 -0700

    latest code

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@ $(document).ready(function(){
 	/* orders.html javascript */
 
 
-	/* patients.html javascript */
+	/*
+	 *
+	 * patients.html javascript
+	 *
+	 */
+
+	/* patient card popovers */
 	for (var i = 11; i >= 0; i--) {
 		var key1 = '#ins-providers-'+i;
 		var key2 = '#medications-'+i;
 
 		$(key1).popover({
-			trigger:'click',
+			trigger:'hover',
 			title:'Insurance Providers',
 			html:true,
-			content:'<ul class="popover-list"><li>Provider 1</li><li>Providers 2</li></ul>',
+			content:'<ul class="popover-list"><li>Provider 1</li><li>Providers 2</li></ul><div><a href="/step1" class="btn btn-success pull-right">Edit</a></div>',
 			placement:'top',
 		});
 
@@ -19,9 +25,31 @@ $(document).ready(function(){
 			trigger:'hover',
 			html:true,
 			title:'Prescribed Medications',
-			content:'<ul class="popover-list"><li>Medication 1</li><li>Medication 2</li><li>Medication 3</li></ul>',
+			content:'<ul class="popover-list"><li>Medication 1</li><li>Medication 2</li><li>Medication 3</li></ul><div><a href="/step1" class="btn btn-success pull-right">Edit</a></div>',
 			placement:'top',
 		});
 	}
+
+	if ($('#patients-page-list').is(":visible")) {
+		$('#patients-list-control').toggleClass('active');
+	}
+	if ($('#patients-page-cards').is(":visible")) {
+		$('#patients-cards-control').toggleClass('active');
+	}
+
+	$('#patients-list-control').click(function() {
+		$('#patients-list-control').addClass('active');
+		$('#patients-cards-control').removeClass('active');
+		$('#patients-page-cards').addClass('hidden');
+		$('#patients-page-list').removeClass('hidden');
+	});
+
+	$('#patients-cards-control').click(function() {
+		$('#patients-cards-control').addClass('active');
+		$('#patients-list-control').removeClass('active');
+		$('#patients-page-list').addClass('hidden');
+		$('#patients-page-cards').removeClass('hidden');
+	});
+
 
 });
